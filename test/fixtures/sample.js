b5eea90cdbceab90f1e4bd53a3b4671962b821bb
Konstantin Käfer <github@kkaefer.com>
2011-06-02 10:02:48 -0400

    add missing test fixture file

---
+++ test/fixtures/sample.js
@@ -0,0 +1,16 @@
+// (c) Development Seed
+
+// Tremendous amounts of whitespace in this file.
+
+function baz(bar)
+
+    // Begin function baz
+    {
+            // Assigning bar to foo
+            var foo = bar;
+
+            // Returning foo.
+            return foo;
+
+    // End function baz
+    }
